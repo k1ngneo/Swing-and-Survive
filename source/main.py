d6f5fb2c3b244f36b4ec6bfbe1b2f0cd7af49280
k1ngneo <k1ng4182k@gmail.com>
2023-02-21 13:31:02 -0500

    changed order of rendering UI and background balls colliding in menu screen

--- source/main.py
+++ source/main.py
@@ -1,13 +1,13 @@
 __version__ = "0.1"
 
 from kivy.app import App
-from kivy.core.window import Window
-from kivy.graphics import Color, Rectangle, Line
+from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import FadeTransition, Screen
 from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.widget import Widget
 from kivy.config import Config
 from game_screen import GameScreen
-from source.scene import Scene
+from scene import Scene
 from user_interface import *
 
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
@@ -19,14 +19,17 @@ Config.write()
 class SettingsScreen(Screen):
     pass
 
+class SceneWidget(Widget):
+    pass
 
 class MenuScreen(Screen):
     __ball_spawn_dt = 0.0
+    scene_widget = ObjectProperty(Widget())
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.scene = Scene(self)
+        self.scene = Scene(self.scene_widget)
         Clock.schedule_interval(self.update, 1.0 / 120.0)
 
     def on_size(self, *args):
