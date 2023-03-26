ba5b28d64416646941a72479495c74dfcd651d29
k1ngneo <13601585+k1ngneo@users.noreply.github.com>
2023-03-26 10:41:57 -0400

    finished summary screen

--- source/main.py
+++ source/main.py
@@ -76,15 +76,30 @@ class SettingsScreen(Screen):
 class SummaryScreen(Screen):
     def __init__(self, **kwargs):
         super(SummaryScreen, self).__init__(**kwargs)
+
         restart_button = Button(text='Play Again', pos_hint={'x': 0.5, 'y': 0.2})
         restart_button.color = (0.1, 0.1, 0.1, 1)
         restart_button.background_color = (0, 0, 0, 0)
+        restart_button.size_hint_y = 0.1
         restart_button.bind(on_press=self.restart)
         self.ids.grid.add_widget(restart_button)
+
+        main_menu_button = Button(text='Main Menu')
+        main_menu_button.color = (0.1, 0.1, 0.1, 1)
+        main_menu_button.background_color = (0, 0, 0, 0)
+        main_menu_button.size_hint_y = 0.1
+        main_menu_button.bind(on_press=self.go_to_main_menu)
+        self.ids.grid.add_widget(main_menu_button)
+
     def restart(self, instance):
         self.manager.get_screen('game').scene.data.score = 0.0
         self.manager.get_screen('game').scene.add_player()
         self.manager.current = 'game'
+
+    def go_to_main_menu(self, instance):
+        self.manager.get_screen('game').scene.data.score = 0.0
+        self.manager.get_screen('game').scene.add_player()
+        self.manager.current = 'menu'
 
 class MenuScreen(Screen):
     __ball_spawn_dt = 0.0
